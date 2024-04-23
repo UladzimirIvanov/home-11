61beeaec9b09513f0dadd07fd72a59e6f8e75cbd
UladzimirIvanov <ivanouum@gmail.com>
2024-04-23 18:05:11 +0300

    Task4 result

--- src/Task4/Main.java
+++ src/Task4/Main.java
@@ -6,77 +6,49 @@ public class Main {
     public static void main(String[] args) {
         /*Ввести 3 строки с консоли. Найти слово, состоящее только из различных символов.
         Если таких слов несколько, найти первое из них*/
-        String userInput = "Helo";
-        char[] arr = userInput.toCharArray();
+
+        Scanner scanner = new Scanner(System.in);
+        String[] arrayUserLine = new String[3];
         boolean compare = false;
 
+        for (int i = 0; i < arrayUserLine.length; i++) {
+            System.out.println("Введите строку " + (i + 1));
+            arrayUserLine[i] = scanner.nextLine();
+        }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    System.out.println("good i = " + arr[i] + " and j = " + arr[j]);
-                    compare = true;
-                    break;
-                } else {
-                    System.out.println("bad i = " + arr[i] + " and j = " + arr[j]);
+        for (int i = 0; i < arrayUserLine.length; i++) {
+            String[] arrayUserWordsInFirstLine = arrayUserLine[i].split(" ");
+            for (int j = 0; j < arrayUserWordsInFirstLine.length; j++) {
+                compare = false;
+                //System.out.println(arrayUserWordsInFirstLine[j]);
+                char[] arrSymbols = arrayUserWordsInFirstLine[j].toCharArray();
+                for (int k = 0; k < arrSymbols.length; k++) {
+                    //System.out.println(arrSymbols[k]);
+                    for (int l = k + 1; l < arrSymbols.length; l++) {
+                        if (arrSymbols[k] == arrSymbols[l]) {
+                            //System.out.println("В строке " + arrayUserLine[i] + " в слове " + arrayUserWordsInFirstLine[j] + " Совпали символы " + arrSymbols[k] + " и " + arrSymbols[l]);
+                            compare = true;
+                            break;
+                        }
+                    }
+                    if (compare == true) {
+                        break;
+                    }
+                }
+                if (compare == false){
+                    System.out.println("В строке " + "\"" + arrayUserLine[i] + "\"" + " в слове " + "\"" + arrayUserWordsInFirstLine[j] + "\"" + " нет повторяющихся символов");
+                    return;
                 }
             }
-            if (compare == true) {
-                break;
-            }
-        }
-        if (compare == false){
-            System.out.println("Искомое слово: " + userInput);
         }
     }
 }
 
 
-
-
-
-
-        /*for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*Scanner scanner = new Scanner(System.in);
-        String[] arrayWords = scanner.nextLine().split(" ");
-
-        boolean compare = false;
-
-        for (int i = 0; i < arrayWords.length; i++) {
-            System.out.println(arrayWords[i]);
-
-            char[] arrayChar = arrayWords[i].toCharArray();
-            while (compare == false) {
-                compare = true;
-                for (int j = 0; j < arrayChar.length - 1; j++) {
-                    if (arrayChar.equals(arrayChar[j+1]))
-
-                    System.out.println(arrayChar[j]);
-
-
-                }
-            }
-            System.out.println();
-        }*/
-
+/*
+if (arrSymbols[j] == arrSymbols[k]){
+compare = true;
+        break;
+        }else {
+        System.out.println("bad i = " + arrSymbols[j] + " and j = " + arrSymbols[k]);
+                    }*/
